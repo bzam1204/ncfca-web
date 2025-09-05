9feb347a84496acf4599b9f674889c01bc0d668c
brunozamorano <bzam1204@gmail.com>
2025-09-05 20:36:49 -0300

    feat: enhance useAdminAffiliations hook to map dependants from affiliations

--- src/hooks/use-admin-affiliations.ts
+++ src/hooks/use-admin-affiliations.ts
@@ -3,10 +3,16 @@
 import { useQuery } from '@tanstack/react-query';
 import { QueryKeys } from '@/infrastructure/cache/query-keys';
 import { getAffiliationsAction } from '@/infrastructure/actions/admin/get-affiliations.action';
+import { Dependant } from '@/domain/entities/dependant.entity';
 
 export function useAdminAffiliations() {
   return useQuery({
     queryKey: QueryKeys.admin.affiliations(),
-    queryFn: () => getAffiliationsAction(),
+    queryFn: async () => {
+      const affiliations = await getAffiliationsAction();
+      for (const affiliation of affiliations) {
+        affiliation.dependants = affiliation.dependants.map((dependant) => new Dependant(dependant));
+      }
+    },
   });
 }
